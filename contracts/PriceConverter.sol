a001bd5523d6ac36701ebb2af37c2cab95c7d4ea
smahdi-dev <seyedmahdi.hosseini24@gmail.com>
2024-01-08 18:07:39 +0330

    getPrice() and getConversionRate() functions' visibility changed to internal

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -5,7 +5,7 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
     
-    function getPrice() public view returns (uint256) {
+    function getPrice() internal view returns (uint256) {
         // Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
@@ -16,7 +16,7 @@ library PriceConverter {
         return uint256(price * 1e10); // 1**10
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
         uint256 ethPrice = getPrice(); // ETH price in USD
         // 3000_000000000000000000 = ETH / USD price
         // 1_000000000000000000 ETH
