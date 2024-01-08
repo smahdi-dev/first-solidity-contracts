a4344072c915addab7291e7e354c4cc7812583de
smahdi-dev <seyedmahdi.hosseini24@gmail.com>
2024-01-08 19:28:54 +0330

    withdraw() function in FindMe.sol resetting funders and funderToAmountFunded and withdrawing from contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -27,5 +27,26 @@ contract FundMe {
         // undo any action before, and send remaining gas back
     }
 
-    // function withdraw() {}
+    function withdraw() public {
+        /* starting point, ending point, step amount */
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reset the array
+        funders = new address[](0);
+
+        // withdraw the funds
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed!");
+
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
+    }
 }
